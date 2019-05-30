7d17f506723c34f6a1b2479052e7df14f7a2c2c9
Paul Sorenson <psoren@umich.edu>
2019-05-29 23:29:20 -0400

    Simple putt course, golf club, and golf ball

---
+++ MagicPuttUnityProject/Assets/Scripts/GolfClubHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class GolfClubHandler : MonoBehaviour
+{
+    public ControllerConnectionHandler _controllerConnectionHandler;
+    public GameObject golfClub;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_controllerConnectionHandler.IsControllerValid())
+        {
+            MLInputController controller = _controllerConnectionHandler.ConnectedController;
+            if (controller.Type == MLInputControllerType.Control)
+            {
+                // For Control, raw input is enough
+                golfClub.transform.position = controller.Position;
+                golfClub.transform.rotation = controller.Orientation;
+            }
+        }
+    }
+}
---
+++ MagicPuttUnityProject/Assets/Scripts/GolfCourseVisualizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class GolfCourseVisualizer : MonoBehaviour
+{
+
+    public MLImageTrackerBehavior _trackerBehavior;
+    public GameObject golfCourse;
+    public GameObject golfBall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLInput.OnTriggerDown += HandleOnTriggerDown;
+    }
+
+    private void HandleOnTriggerDown(byte controllerId, float triggerValue)
+    {
+        if (!_trackerBehavior.IsTracking)
+            return;
+
+        golfCourse.SetActive(true);
+        golfCourse.transform.position = _trackerBehavior.transform.position;
+        golfCourse.transform.rotation = _trackerBehavior.transform.rotation;
+
+        golfBall.transform.position = new Vector3(_trackerBehavior.transform.position.x, _trackerBehavior.transform.position.y + 0.5f, _trackerBehavior.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
