dfcafa7fe76f836eab40c248926c8ae7acef2b81
Paul Sorenson <psoren@umich.edu>
2019-06-19 00:11:15 -0400

    Minor UI changes

---
+++ MagicPuttArCoreClient/Assets/Scripts/UI/BottomTrayPullHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class BottomTrayPullHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool buttonPressed;
+    float travel;
+    Vector2 startPoint;
+    const float maxTravel = 400;
+    public GameObject drawer;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startPoint = eventData.position;
+        buttonPressed = true;
+    }
+
+    private void Start()
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if(travel > 70)
+        {
+            //OPEN 
+            drawer.SetActive(true);
+        }
+        else if(travel < -70)
+        {
+            //CLOSE
+            drawer.SetActive(false);
+        }
+        buttonPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonPressed && (Input.touchCount > 0))
+        {
+            travel = Input.GetTouch(0).position.y - startPoint.y;
+        }
+    }
+
+}
