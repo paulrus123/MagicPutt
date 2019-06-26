65f3fd06fb96100cd2f9a66294c895e424119900
Paul Sorenson <psoren@umich.edu>
2019-06-26 15:59:27 -0400

    Infopage created on phone

---
+++ MagicPuttArCoreClient/Assets/Scripts/UI/ButtonHandlers/InfoButttonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoButttonHandler : MonoBehaviour
+{
+    [SerializeField]
+    UIHandlerMain uiHandler = default;
+
+    public void ButtonPressed()
+    {
+        uiHandler.SetState(UIHandlerMain.UIState.INFO);
+    }
+}
