ecf7a8aca33a0a53144721889cf7abb11e5f290e
Paul Sorenson <psoren@umich.edu>
2019-07-18 22:27:15 -0400

    Add scorecard to magic leap

--- MagicPuttUnityProject/Assets/Scripts/Score/ScoreHandler.cs
+++ MagicPuttUnityProject/Assets/Scripts/Score/ScoreHandler.cs
@@ -1,21 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreHandler : MonoBehaviour
 {
+    const int numHoles = 9;
+
     //Current hole the user is playing on
     public int currentHole = 1;
 
     //Pars for each hole
     [SerializeField]
-    public int[] pars = new int[9];
+    public int[] pars = new int[numHoles];
+    private int[] lastPars = new int[numHoles];
+
+    [SerializeField]
+    TextMesh parText = default;
+
+    [SerializeField]
+    TextMesh scoreText = default;
 
     //Scores for Each hole
-    public int[] scores = new int[9];
+    public int[] scores = new int[numHoles];
+    private int[] lastScores = new int[numHoles];
+
+    float currTime = 0;
+
+
+    private void Update()
+    {
+        //Update scorecard every 1/2 second
+        currTime += Time.deltaTime;
+
+        if (currTime > 0.5)
+        {
+            UpdateParText();
+            UpdateScoreText();
+            currTime = 0;
+        }
+    }
 
     public void IncrementStroke()
     {
         scores[currentHole-1]++;
+    }
+
+    void UpdateParText()
+    {
+        string _text = "";
+
+        for(int i =0; i < pars.Length; i++)
+        {
+            _text += pars[i].ToString() + "\n\n";
+        }
+        parText.text = _text;
+    }
+
+    void UpdateScoreText()
+    {
+        string _text = "";
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] == 0)
+            {
+                _text += "\n\n";
+            }
+            else
+            {
+                _text += scores[i].ToString() + "\n\n";
+            }
+        }
+        scoreText.text = _text;
     }
 }
---
+++ MagicPuttUnityProject/Assets/Scripts/Score/Scorecard/Scorecard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Scorecard : MonoBehaviour
+{
+    [SerializeField]
+    GameObject card = default;
+
+    private MLInputController _controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_controller.Touch1PosAndForce.z > 0.0f)
+        {
+            card.SetActive(true);
+        }
+        else
+        {
+            card.SetActive(false);
+        }
+    }
+}
